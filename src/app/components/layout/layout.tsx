23b4a8d7a8d519177fd5e87eb57f3f6289707352
Maryna Doroshenko <thesmirnovalex@gmail.com>
2023-07-08 21:35:24 +0200

    create component 'MainMenu'

--- src/app/components/layout/layout.tsx
+++ src/app/components/layout/layout.tsx
@@ -5,11 +5,19 @@ import styles from './layout.module.css';
 /* eslint-disable-next-line */
 export interface LayoutProps {}
 
+const portfolio = [
+  { title: 'Scetches1', slug: 'scetches1' },
+  { title: 'Scetches2', slug: 'scetches2' },
+  { title: 'Scetches3', slug: 'scetches3' },
+];
+
 export function Layout(props: LayoutProps) {
   return (
     <div className={styles['container']}>
-      <MainMenu />
-      <Outlet />
+      <MainMenu  portfolio={portfolio}/>
+      <div className={styles['page']}>
+        <Outlet />
+      </div>
     </div>
   );
 }
--- src/app/components/main-menu/main-menu.tsx
+++ src/app/components/main-menu/main-menu.tsx
@@ -1,22 +1,62 @@
 import { Link } from 'react-router-dom';
 
 import styles from './main-menu.module.css';
+// import './main-menu.module.css';
+
+import logo from '../../../../images/ginko.jpeg';
+
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 
 /* eslint-disable-next-line */
-export interface MainMenuProps {}
+
+export interface PortfolioMenuItem {
+  title: string;
+  slug: string;
+}
+export interface MainMenuProps {
+  portfolio: Array<PortfolioMenuItem>;
+}
 
 export function MainMenu(props: MainMenuProps) {
   return (
-    <div className={styles['container']}>
-      <ul>
-        <li>
-          <Link to="/">MainMenu root</Link>
-        </li>
-        <li>
-          <Link to="/project">ProjectPage root</Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className={styles['placeholder']}></div>
+      <div className={styles['container']}>
+        <Link to="/">
+          <img className={styles['logo']} src={logo} alt="logo luba" />
+        </Link>
+        <ul className={styles['menu']}>
+          <li className={styles['menu_list']}>
+            Portfolio
+            <ul className={styles['drop']}>
+              {props.portfolio.map((portfolioItem) => (
+              <li  key={portfolioItem.slug}>
+                <Link to={`/${portfolioItem.slug}`}>{portfolioItem.title}</Link>
+              </li>
+              ))}
+            </ul>
+          </li>
+          <li className={styles['menu_list']}>About me</li>
+          <li className={styles['menu_list']}>Faq</li>
+          <li className={styles['menu_list']}>Contacts</li>
+        </ul>
+
+        <ul className={styles['contacts']}>
+          <li className={styles['contacts_item']}>
+            <a href="https://www.instagram.com/" target="blank">
+              <InstagramIcon />
+            </a>
+          </li>
+          <li className={styles['contacts_item']}>
+            <a href="https://www.linkedin.com/" target="blank">
+              <LinkedInIcon />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 

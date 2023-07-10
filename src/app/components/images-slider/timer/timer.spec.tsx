bd3b536dabaabd5296c8fd750d2f0b87496b1c68
Kirill Sudarushkin <kirill@bigmilk.ru>
2023-07-10 22:13:35 +0200

    fix tests and enable tests in workflow

--- src/app/components/images-slider/timer/timer.spec.tsx
+++ src/app/components/images-slider/timer/timer.spec.tsx
@@ -1,10 +1,17 @@
 import { render } from '@testing-library/react';
-
+import { Seconds } from 'src/app/utils/units';
 import Timer from './timer';
 
 describe('Timer', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Timer />);
+    const { baseElement } = render(
+      <Timer
+        play={false}
+        pause={false}
+        duration={2 as Seconds}
+        finish={() => {}}
+      />
+    );
     expect(baseElement).toBeTruthy();
   });
 });

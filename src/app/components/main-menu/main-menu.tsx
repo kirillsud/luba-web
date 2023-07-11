ef264e0713a2db5fb4c47559828adf9cc5311d01
verymarin <138394424+verymarin@users.noreply.github.com>
2023-07-11 22:47:34 +0200

    LUBA-1: Create component 'MainMenu' (#4)

--- src/app/components/main-menu/main-menu.tsx
+++ src/app/components/main-menu/main-menu.tsx
@@ -2,21 +2,69 @@ import { Link } from 'react-router-dom';
 
 import styles from './main-menu.module.css';
 
-/* eslint-disable-next-line */
-export interface MainMenuProps {}
+import logo from '../../../../images/ginko.jpeg';
+
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+export interface PortfolioMenuItem {
+  title: string;
+  url: string;
+}
+export interface MainMenuProps {
+  portfolio: Array<PortfolioMenuItem>;
+}
 
 export function MainMenu(props: MainMenuProps) {
   return (
-    <div className={styles['container']}>
-      <ul>
-        <li>
-          <Link to="/">MainMenu root</Link>
-        </li>
-        <li>
-          <Link to="/project">ProjectPage root</Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className={styles['placeholder']}></div>
+      <div className={styles['container']}>
+        <Link to="/">
+          <img className={styles['logo']} src={logo} alt="logo luba" />
+        </Link>
+        <ul className={styles['menu']}>
+          <li className={styles['menu_list']}>
+            Portfolio
+            <ul className={styles['drop']}>
+              {props.portfolio.map((portfolioItem) => (
+                <li key={portfolioItem.url}>
+                  <Link to={`/portfolio/${portfolioItem.url}`}>
+                    {portfolioItem.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li className={styles['menu_list']}>
+            <Link to="/aboutme">About me</Link>
+          </li>
+          <li className={styles['menu_list']}>
+            <Link to="/faq">Faq</Link>
+          </li>
+          <li className={styles['menu_list']}>
+            <Link to="/contacts">Contacts</Link>
+          </li>
+        </ul>
+
+        <ul className={styles['contacts']}>
+          <li className={styles['contacts_item']}>
+            <a
+              href="https://www.instagram.com/"
+              target="blank"
+              title="Instagram"
+            >
+              <InstagramIcon />
+            </a>
+          </li>
+          <li className={styles['contacts_item']}>
+            <a href="https://www.linkedin.com/" target="blank" title="LinkedIn">
+              <LinkedInIcon />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 

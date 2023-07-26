f0ac352fe44caabca4125c28941804fc367a0546
Kirill Sudarushkin <kirill@bigmilk.ru>
2023-07-26 19:42:37 +0200

    fix build errors

--- src/app/pages/portfolio-page/portfolio-page.tsx
+++ src/app/pages/portfolio-page/portfolio-page.tsx
@@ -1,41 +1,36 @@
-import React from 'react'
-import styles from './portfolio-page.module.css'
+import React from 'react';
+import styles from './portfolio-page.module.css';
 import { Link } from 'react-router-dom';
 
 export interface PortfolioPageProps {
-    portfolio: { img: string; id: number }[];
-    title: string;
-  }
-
-function PortfolioPage(props: PortfolioPageProps) {
-
- 
-    return (
-        <div className={styles.portfolio_page}>
-          <h1>{props.title}</h1>
-          <div className={styles.container}>
-            <div>
-              {props.portfolio &&
-                props.portfolio.map((pic, index) => {
-                  return (
-                    <Link className={styles.link} to={`/project/${pic.id}`}>
-                    <div
-                      className={styles.img}
-                      key={pic.id}
-                    >
-                      <img
-                        src={pic.img}
-                        alt={`my project ${pic.id}`}
-                        className={styles.img}
-                      />
-                    </div>
-                    </Link>
-                  );
-                })}
-            </div>
-          </div>
-        </div>
-      );
+  portfolio: { img: string; id: string }[];
+  title: string;
 }
 
-export default PortfolioPage
+function PortfolioPage(props: PortfolioPageProps) {
+  return (
+    <div className={styles.portfolio_page}>
+      <h1>{props.title}</h1>
+      <div className={styles.container}>
+        <div>
+          {props.portfolio &&
+            props.portfolio.map((pic, index) => {
+              return (
+                <Link className={styles.link} to={`/projects/${pic.id}`}>
+                  <div className={styles.img} key={pic.id}>
+                    <img
+                      src={pic.img}
+                      alt={`my project ${pic.id}`}
+                      className={styles.img}
+                    />
+                  </div>
+                </Link>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PortfolioPage;

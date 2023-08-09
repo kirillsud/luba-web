29f9ca557b92020e0ea8e5a5533681652514be4a
Kirill Sudarushkin <kirill@bigmilk.ru>
2023-08-09 14:45:00 +0200

    Add coverage report to PR comments (#18)

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,7 @@ export default {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: './coverage/luba-web',
+  coverageReporters: ['clover'],
   //  testMatch: [
   //    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
   //    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
